5f67182ef0a44e2eacba2311b21fe2e2f8a36ef0
johndavedecano <johndavedecano@gmail.com>
2012-03-24 23:18:43 +0000

    n

---
+++ upload.php
@@ -0,0 +1,28 @@
+<?php 
+require('upload_class.php');
+$upload = new Upload();
+?>
+<html>
+<head>
+    <title>Upload Class</title>
+    <style>
+    #form-holder { width:500px; margin:auto;}
+    </style>
+</head>
+<body>
+<?php
+if(isset($_POST)){
+    $dir = dirname(__FILE__).'/images';
+    $upload->set_dir($dir);
+    $upload->set_max(200000000);
+    $upload->process_file($_FILES['file']);
+}
+?>
+<div id="form-holder">
+    <form action="" method="post" enctype="multipart/form-data">
+    <p>Select a file:<br /><input type="file" name="file" id="file" class="" /></p>
+    <div style="text-align:center;"><input type="submit" name="upload" value="Upload"/></div>
+    </form>
+</div>
+</body>
+</html>
